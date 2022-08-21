b58b676967943d73e879320edbdee799dd0afec1
Shashank <shashank.srinivasg@gmail.com>
2022-08-21 16:23:33 +0530

    final commit

--- week5/index.js
+++ week5/index.js
@@ -1,20 +1,4 @@
-// function colorGenerator(){
-//     let red = Math.floor(Math.random() * 256)
-//     let green = Math.floor(Math.random() *256 )
-//     let blue = Math.floor(Math.random() *256 )
 
-//     let color = document.getElementById("name")
-//     let btn = document.querySelector(".btn")
-
-    
-//     let container = document.getElementById("container")
-
-
-//     color.innerHTML = `rgb(${red},${green},${blue})`
-//     container.style.backgroundColor = `rgb(${red},${green},${blue})`
-//     btn.style.color = `rgb(${red},${green},${blue})`
-
-// }
 
 const btnGenerate = document.querySelector("#btn-gnrt");
 
