c8825ab6c8c2a919c6f05f65948e7177927eb9c8
Shashank <shashank.srinivasg@gmail.com>
2022-09-04 17:28:10 +0530

    last commit

---
+++ week7/README.md
@@ -0,0 +1,4 @@
+Weather API Application
+
+API used is from https://openweathermap.org/current
+The key is unique and to be generated from Open Weather Map to get the access from the API
--- week7/logic.js
+++ week7/logic.js
@@ -1,13 +1,16 @@
 // const url = https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API key}&units=metric
 // let API_KEY = ee94cbfc21c8d66cca0d823a3262ddd7;
 
-const API_KEY = `ee94cbfc21c8d66cca0d823a3262ddd7`;
+const API_KEY = `ee94cbfc21c8d66cca0d823a3262ddd7`; //API Key generated from openweather website
 
 const form = document.querySelector("form")
 const search = document.querySelector("#input")
 const weather = document.querySelector("#weather")
+const container = document.querySelector("container")
 
 const getWeather = async(city) => {
+
+    weather.innerHTML = `<h2>Loading...</h2>`
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url)
@@ -17,6 +20,11 @@ const getWeather = async(city) => {
 }
 
 const showWeather = (data) => {
+
+    if(data.cod == '404'){
+        weather.innerHTML = `<h2>City Not Found</h2>`
+        return;
+    }
     weather.innerHTML = `
     <div>
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
@@ -26,7 +34,9 @@ const showWeather = (data) => {
         <h4>${data.weather[0].main}</h4>
     </div>
     `
+    
 
+    
 }
 
 form.addEventListener("submit", function(event){
