931d971a01c98f61f039731f82a28b217f264cbb
Shashank <shashank.srinivasg@gmail.com>
2022-09-11 15:36:01 +0530

    week 8 and index update

---
+++ week8/README.md
@@ -0,0 +1,1 @@
+Week 8 task is to make Calculator based on Object oriented programming
--- week8/index.js
+++ week8/index.js
@@ -1,33 +1,44 @@
-let num1 = parseFloat(prompt("Enter the first number: "))
-let num2 = parseFloat(prompt("Enter the second number: "))
 
-let input = prompt("Enter the operation to be performed[ +,-,*,/ ]:")
+function validate(){
+    let num1 = parseFloat(prompt("Enter the first number: "))
+    let num2 = parseFloat(prompt("Enter the second number: "))
+
+    let input = prompt("Enter the operation to be performed[ +,-,*,/ ]:")
+
+    let text = document.querySelector('.text')
 
 let calculator = {
     add(num1,num2){
         x = num1+num2
         console.log(x)
+        text.innerHTML = `The addition operation is equal to ${x}`
     },
     subtract(num1,num2){
         y = num1-num2
         console.log(y)
+        text.innerHTML = `The Subtraction operation is equal to ${y}`
     },
     multiplication(num1,num2){
         a = num1*num2
         console.log(a)
+        text.innerHTML = `The Multiplication operation is equal to ${a}`
     },
     division(num1,num2){
         b = num1/num2
         console.log(b)
+        text.innerHTML = `The Division operation is equal to ${b}`
     }
 }
+    if (input == '+'){
+        calculator.add(num1,num2)
+    } else if(input == '-'){
+        calculator.subtract(num1,num2)
+    } else if(input == '*'){
+        calculator.multiplication(num1,num2)
+    } else if(input == '/'){
+        calculator.division(num1,num2)
+    } else console.log("Please enter correct input")
 
-if (input == '+'){
-    calculator.add(num1,num2)
-} else if(input == '-'){
-    calculator.subtract(num1,num2)
-} else if(input == '*'){
-    calculator.multiplication(num1,num2)
-} else if(input == '/'){
-    calculator.division(num1,num2)
-} else console.log("Please enter correct input")
+
+
+}
